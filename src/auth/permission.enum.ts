c6fa7c84be9af4eda955e58dae1f56876397876e
Daniel Fernández <daniel.fdez.fdez@gmail.com>
2026-03-23 21:06:32 +0100

    feat: Add Permission enum for authorization control

---
+++ src/auth/permission.enum.ts
@@ -0,0 +1,13 @@
+export enum Permission {
+  ASK = 'ask',
+  SUMMARIZE = 'summarize',
+  TRANSLATE = 'translate',
+  ENTITY_EXTRACTION = 'entity-extraction',
+  KEY_POINTS = 'key-points',
+  KEYWORDS = 'keywords',
+  PROJECTS = 'projects',
+  UPLOAD = 'upload',
+  EXPORT = 'export',
+  WRITE = 'write',
+  DELETE = 'delete',
+}
